9e595d3892918e56d4c28687c0b1e6daed1a6956
hevertoncastro <heverton@hotmail.com.br>
2017-01-07 23:13:27 -0200

    Learning propTypes

---
+++ m01/learning-state/src/app.js
@@ -0,0 +1,21 @@
+'use strict'
+
+import React, { Component } from 'react'
+import Button from './button'
+
+import './css/style.css'
+
+class App extends Component {
+
+  render () {
+    return (
+      <div>
+        <Button>
+          Clique em mim
+        </Button>
+      </div>
+    )
+  }
+}
+
+export default App
