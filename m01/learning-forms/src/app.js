ba9bd49357e1562a8cbefd4f5d8ba3bbf85230ec
hevertoncastro <heverton@hotmail.com.br>
2017-03-25 16:54:09 -0300

    setState is async

--- m01/learning-forms/src/app.js
+++ m01/learning-forms/src/app.js
@@ -8,56 +8,30 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      value: '2'
-      // checked: true
+      checked: false,
+      showContent: false
     }
-
   }
 
   render () {
     return (
       <div>
-        <form 
-          onSubmit={(e) => {
-            e.preventDefault()
-            console.log('event', e)
-          }}
-
-          onChange={(e) => {
-            console.log('name', e.target.name)
-            console.log('value', e.target.value)
-          }}
-        >
-          <input type='text' value={this.state.value} onChange={(e) => {
-              console.log(e.nativeEvent)
+        <label>
+          <input
+            type='checkbox'
+            checked={this.state.checked}
+            onChange={() => {
               this.setState({
-                value: e.target.value
+                checked: !this.state.checked
+              }, () => {
+                this.setState({
+                  showContent: this.state.checked
+                })
               })
-            }} />
-
-          <label>
-            <input type='checkbox'
-              value='my-checkbox'
-              defaultChecked />
-            checkbox
-          </label>
-
-          <input type='radio' name='rd' value='1' defaultChecked /> Radio 1
-          <input type='radio' name='rd' value='2' /> Radio 2
-
-          <select multiple value={['1', '2']} onChange={(e) => {
-              this.setState({
-                value: e.target.value
-              })
-            }}>
-            <option value='1'>Option 1</option>
-            <option value='2'>Option 2</option>
-            <option value='3'>Option 3</option>
-          </select>
-
-          <textarea name='name' defaultValue={'testando\nasdasdas'} />
-          <button type='submit'>Send</button>
-        </form>
+            }}
+          /> Show content
+        </label>
+        {this.state.showContent && <div>Here I am.</div>}
       </div>
     )
   }
