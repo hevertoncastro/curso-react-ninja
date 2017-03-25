3e857fd2b71ccb9ac3c846fb5ba57b1a16240924
hevertoncastro <heverton@hotmail.com.br>
2017-03-25 16:26:41 -0300

    Learning forms in React

--- m01/learning-forms/src/app.js
+++ m01/learning-forms/src/app.js
@@ -8,8 +8,8 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      value: 'Initial value',
-      checked: true
+      value: '2'
+      // checked: true
     }
 
   }
@@ -17,7 +17,17 @@ class App extends Component {
   render () {
     return (
       <div>
-        <form>
+        <form 
+          onSubmit={(e) => {
+            e.preventDefault()
+            console.log('event', e)
+          }}
+
+          onChange={(e) => {
+            console.log('name', e.target.name)
+            console.log('value', e.target.value)
+          }}
+        >
           <input type='text' value={this.state.value} onChange={(e) => {
               console.log(e.nativeEvent)
               this.setState({
@@ -35,6 +45,18 @@ class App extends Component {
           <input type='radio' name='rd' value='1' defaultChecked /> Radio 1
           <input type='radio' name='rd' value='2' /> Radio 2
 
+          <select multiple value={['1', '2']} onChange={(e) => {
+              this.setState({
+                value: e.target.value
+              })
+            }}>
+            <option value='1'>Option 1</option>
+            <option value='2'>Option 2</option>
+            <option value='3'>Option 3</option>
+          </select>
+
+          <textarea name='name' defaultValue={'testando\nasdasdas'} />
+          <button type='submit'>Send</button>
         </form>
       </div>
     )
