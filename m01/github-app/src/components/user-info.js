ae773a64d6df3f459bc395029c14dc757180de01
hevertoncastro <heverton@hotmail.com.br>
2017-03-26 20:02:29 -0300

    Starting  an app using Github API

---
+++ m01/github-app/src/components/user-info.js
@@ -0,0 +1,21 @@
+'use strict'
+
+import React from 'react'
+
+const UserInfo = () => (
+  <div className='user-info'>
+    <img src='https://avatars0.githubusercontent.com/u/7852959?v=3' />
+
+    <h1 className='username'>
+      <a href='https://github.com/hevertoncastro'>Heverton Castro</a>
+    </h1>
+
+    <ul className='repos-info'>
+      <li> - Repositórios: 33</li>
+      <li> - Seguidores: 15</li>
+      <li> - Seguindo: 53</li>
+    </ul>
+  </div>
+)
+
+export default UserInfo
